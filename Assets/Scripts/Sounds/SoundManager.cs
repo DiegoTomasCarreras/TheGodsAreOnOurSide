73f5863fac89a6e389359aaf26b82ef99b753575
Diego Carreras <diegotomascarreras@gmail.com>
2021-11-08 18:19:06 -0300

    Added click sound to all buttons

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip clickSound;
+    public AudioSource source;
+    public GameObject clickSoundPrefab;
+    public GameObject cameraLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayClickSound()
+    {
+        source.PlayOneShot(clickSound);
+    }
+    public void PlayClickSound2()
+    {
+        Instantiate(clickSoundPrefab, cameraLocation.transform.position, cameraLocation.transform.rotation);
+    }
+}
